8cb4611f682707c3d80c463b418940d2b604cde2
QutaibaAbualrob <qotaibawaleed@gmail.com>
2025-12-26 19:08:59 +0200

    Saving all resaults

--- backend/true_caller_api/true_caller_api/settings.py
+++ backend/true_caller_api/true_caller_api/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'true_caller_app',
 ]
 
 MIDDLEWARE = [
--- backend/true_caller_api/true_caller_app/models.py
+++ backend/true_caller_api/true_caller_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class phone():
+    full_name = models.CharField(max_length=255)
+    number = models.IntegerField()
+        

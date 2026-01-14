a382546a7e2c2c20ca1636a64b9051f48077dbfc
QutaibaAbualrob <qotaibawaleed@gmail.com>
2026-01-15 00:53:09 +0300

    Delete old test files

--- tssts/test_search.py
+++
@@ -1,8 +0,0 @@
-
-import requests
-BASE_URL = "http://localhost:5000"
-
-def test_search_existing(auth_token):
-    headers={"Authorization":f"Bearer {auth_token}"}
-    r = requests.get(f"{BASE_URL}/search/0591234567", headers=headers)
-    assert r.status_code in [200,404]
